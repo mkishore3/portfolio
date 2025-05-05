6dc4255573010298e4598a161c2a244013cbb642
mkishore3 <mkishore3@users.noreply.github.com>
2025-05-05 00:21:14 +0000

    deploy: dc17ce623b37032ad4a7337581470be2f42a64a6

--- meta/main.js
+++ meta/main.js
@@ -90,9 +90,79 @@ async function loadData() {
       dl.append('dd').text(averageFileLength + " lines");
   }
   
-  let data = await loadData();
-  let commits = processCommits(data);
   
-  renderCommitInfo(data, commits);
 
-  //random comment
+  function renderScatterPlot(data, commits) {
+    const width = 1000;
+    const height = 600;
+  
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  
+    const usableArea = {
+      top: margin.top,
+      right: width - margin.right,
+      bottom: height - margin.bottom,
+      left: margin.left,
+      width: width - margin.left - margin.right,
+      height: height - margin.top - margin.bottom,
+    };
+  
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .style('overflow', 'visible');
+  
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(commits, d => d.datetime))
+      .range([usableArea.left, usableArea.right])
+      .nice();
+  
+    const yScale = d3.scaleLinear()
+      .domain([0, 24])
+      .range([usableArea.bottom, usableArea.top]);
+  
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    // const yAxis = d3.axisLeft(yScale);
+    const yAxis = d3
+        .axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+    // Add X axis
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${usableArea.bottom})`)
+      .call(xAxis);
+  
+    // Add Y axis
+    svg
+      .append('g')
+      .attr('transform', `translate(${usableArea.left}, 0)`)
+      .call(yAxis);
+  
+    // Dots
+    svg.append('g')
+      .attr('class', 'dots')
+      .selectAll('circle')
+      .data(commits)
+      .join('circle')
+      .attr('cx', d => xScale(d.datetime))
+      .attr('cy', d => yScale(d.hourFrac))
+      .attr('r', 4)
+      .attr('fill', 'steelblue');
+  }
+  
+  
+   
+let data = await loadData();
+let commits = processCommits(data);
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
